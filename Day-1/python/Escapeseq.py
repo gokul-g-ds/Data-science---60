0df0a09d5ddce29b76f37be5dd99bde409ef6492
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-20 10:38:15 +0530

    Escapeseq.py

---
+++ Day-1/python/Escapeseq.py
@@ -0,0 +1,13 @@
+print("Hello world")
+print("This is\nEscapeSeq_ex")
+print("Tab\tex")
+print('It\'s python session')
+print("Hi,\"This is double quote ex\"")
+
+
+#Hello world
+#This is
+#EscapeSeq_ex
+#Tab	ex
+#It's python session
+#Hi,"This is double quote ex

1c2a2612a72169aac4fef8d44125d954a31bf35f
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-20 11:07:13 +0530

    Datatypes.py

---
+++ Day-1/python/Datatypes.py
@@ -0,0 +1,16 @@
+name = "python"
+year = 1991
+age = 35
+version = 31.6
+print(name)
+#print(type(name))
+print(year)
+#print(type(year))
+print(age)
+print(version)
+#print(type(version))
+
+#python
+#1991
+#35
+#31.6

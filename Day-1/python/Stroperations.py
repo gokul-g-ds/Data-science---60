72305847698e9cf32b4d5439ef3b07ba4ec14800
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-20 14:28:35 +0530

    Stringoperations.py

---
+++ Day-1/python/Stroperations.py
@@ -0,0 +1,13 @@
+stars = "*"
+greetings = "happy" + "holidays"
+print(stars * 10) #str replication 
+print(stars + greetings) 
+print(stars * 10)
+
+
+'''
+**********
+*happyholidays
+**********str concatination
+...
+

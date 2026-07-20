dcc2d3490be20e7ec4ebf9c026cc01ef2efc3f36
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-20 13:30:07 +0530

    Augumented asssignment .py

---
+++ Day-1/python/Augumented asssignment .py
@@ -0,0 +1,33 @@
+a = 10
+a += 5
+print(a)
+a -= 5
+print(a)
+a *= 3
+print(a)
+a **= 2
+print(a)
+a /= 2
+print(a)
+
+# add intrest with bank balance 
+
+name = "canara"
+balance = 100
+print("Before intrest")
+print(name, balance)
+print("After intrest")
+balance += (7/100 * balance) #balance = balance + (7/100 * balance)
+print(balance)
+
+'''
+15
+10
+30
+900
+450.0
+Before intrest
+canara 100
+After intrest
+107.0
+'''

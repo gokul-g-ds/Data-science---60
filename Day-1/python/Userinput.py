72f1bcfa954db63623939e700b837abeee03c460
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-20 14:41:14 +0530

    Userinput.py

---
+++ Day-1/python/Userinput.py
@@ -0,0 +1,17 @@
+name = input("Enter the name :")
+print("hello",name)
+num = float(input("Enter the num : "))
+print(num)
+a = int(input("Enter the a :"))
+b = int(input("Enter the b :"))
+print(a+b)
+
+
+
+#Enter the name :Gokul
+#hello Gokul
+#Enter the num : 3.56
+#3.56
+#Enter the a :2
+#Enter the b :3
+#5

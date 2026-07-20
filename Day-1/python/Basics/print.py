61e58abec8c069524d26249df12851445830349b
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-20 10:10:24 +0530

    Create print.py

---
+++ Day-1/python/Basics/print.py
@@ -0,0 +1,22 @@
+print("hello gokul")
+#hello gokul
+
+print("hello gokul")
+print("today is day 1")
+#hello gokul
+#today is day
+
+# using End argument 
+print("hello gokul",end = "-")
+print("today is day 1")
+#hello gokul-today is day 1
+
+#using sep argument 
+print("hello gokul","today is day 1")
+#hello gokul today is day 1
+
+#Both end and sep argument 
+print("hello gokul","today is day 1",end = "--",sep = "-")
+print("started",end = "!")
+#hello gokul-today is day 1--started!
+

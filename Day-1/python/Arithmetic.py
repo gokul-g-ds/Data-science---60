61716514e4b1fed4cc2285c6286d4291263e6b1a
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-20 12:06:13 +0530

    Arithmetic.py

---
+++ Day-1/python/Arithmetic.py
@@ -0,0 +1,30 @@
+a = 10
+b = 5
+print(a + b)
+print(type(a + b))
+print(a - b)
+print(type(a - b))
+print(a * b)
+print(a / b)
+print(type(a / b))
+print(a // b)
+print(type(a // b))
+print(a % b)
+print(type(a % b))
+print(a ** b)
+
+'''
+15
+<class 'int'>
+5
+<class 'int'>
+50
+2.0
+<class 'float'>
+2
+<class 'int'>
+0
+<class 'int'>
+100000
+'''
+

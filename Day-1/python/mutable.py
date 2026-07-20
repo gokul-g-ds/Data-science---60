cde79b690e81e26bb075abc9b2a79b659f3fb0bd
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-20 11:51:39 +0530

    mutable.py

---
+++ Day-1/python/mutable.py
@@ -0,0 +1,19 @@
+name = "python"
+print(name)
+print(type(name))
+
+#python
+#<class 'str'>
+
+name = "python"
+print(name)
+print(type(name))
+name = 1981
+print(type(name))
+print(name)
+
+#python
+#<class 'str'>
+#<class 'int'>
+#1981
+

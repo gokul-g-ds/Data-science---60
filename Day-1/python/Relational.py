38422ac4a77e5641c03ebb3a9e1529f4ff332e17
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-20 12:16:09 +0530

    Relational.py

---
+++ Day-1/python/Relational.py
@@ -0,0 +1,23 @@
+a = 10
+b = 20 
+c = "python"
+d = "Python"
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(c > d)
+print (c == d)
+
+'''
+False
+True
+False
+True
+False
+True
+True
+False
+'''

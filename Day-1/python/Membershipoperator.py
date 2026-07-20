196e5633216b361ab2a510ea7bde04405b4a0877
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-20 13:52:47 +0530

    Membershipoperator.py

---
+++ Day-1/python/Membershipoperator.py
@@ -0,0 +1,22 @@
+lst = [ 62,63,64,65]
+print(64 in lst)
+print(61 in lst)
+numbers = {1,2,3,4,5}
+print(5 in numbers)
+print(4 not in numbers)
+name = "Python"
+print("py" not in name)
+print("Py" in name)
+fruit = {"apple","mango"} #set
+print("goa" in fruit)
+
+
+...
+True
+False
+True
+False
+True
+True
+False
+...

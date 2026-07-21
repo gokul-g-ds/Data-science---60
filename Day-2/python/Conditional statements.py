d41ae3fc367e8468e647d8c19d5683f47bbe8b6b
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-21 10:33:58 +0530

    Conditional statements.py

---
+++ Day-2/python/Conditional statements.py
@@ -0,0 +1,26 @@
+num = int(input("Enter the num = "))
+print("Verifying the num is pos or neg")
+if(num > 0):
+    print("The given num is Positive")
+elif(num < 0):
+    print("the given num is negative")
+else:
+    print(" the num is zero ")
+
+#for positive
+'''Enter the num = 23
+Verifying the num is pos or neg
+The given num is Positive'''
+
+#for negative
+'''Enter the num = -23
+Verifying the num is pos or neg
+the given num is negative'''
+
+#zero
+'''Enter the num = 0
+Verifying the num is pos or neg
+ the num is zero 
+
+=== Code Execution Successful ==='''
+

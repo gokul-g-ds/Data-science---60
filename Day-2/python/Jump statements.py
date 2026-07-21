8f926d08cb4ec01ca8d0af3c53d420c85a9a161b
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-21 16:02:56 +0530

    Jump statements.py

---
+++ Day-2/python/Jump statements.py
@@ -0,0 +1,54 @@
+# break 
+
+for i in range(1, 11):
+    if i == 6:
+        break
+
+    print(i)
+
+'''1
+2
+3
+4
+5'''
+
+#continue
+
+for i in range (1,11):
+    if(i == 5):
+        continue
+    print(i)
+
+'''
+1
+2
+3
+4
+6
+7
+8
+9
+10'''
+
+=== Code Execution Successful ===
+
+#pass
+
+for i in range (1,11):
+    if(i == 5):
+     pass
+    print(i)
+
+
+'''1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+=== Code Execution Successful ==='''

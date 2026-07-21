2283494dcb9483594eda65e03d71d4410397a10a
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-21 09:53:19 +0530

    Average num.py

---
+++ Day-2/python/Average num.py
@@ -0,0 +1,13 @@
+mark1 = float(input("Enter the phy marks :"))
+mark2 = float(input("Enter the mat marks :"))
+mark3 = float(input("Enter the che marks :"))
+TM = mark1+mark2+mark3
+avg = TM/3
+print(avg)
+
+#Enter the phy marks :34.5
+#Enter the mat marks :56.8
+#Enter the che marks :54
+#48.43333333333334
+
+

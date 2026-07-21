338b4620a436d03ee6213f65607b21a1ae72cd66
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-21 16:32:33 +0530

    Nested loop.py

---
+++ Day-2/python/Nested loop.py
@@ -0,0 +1,13 @@
+n = int(input("enter the number n :"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end = "")
+        
+    print()
+
+'''enter the number n :5
+1
+12
+123
+1234
+12345'''

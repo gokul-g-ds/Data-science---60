22559cc25f9e45c39d20146d34c932e714328e95
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-21 16:36:24 +0530

    Printing * nestedloop.py

---
+++ Day-2/python/Printing * nestedloop.py
@@ -0,0 +1,20 @@
+n = int(input("no of n is :"))
+for i in range(1,n):
+    for j in range(1,i+1):
+        print("*",end = "")
+        
+    print()    
+        
+'''
+no of n is :10
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********
+
+=== Code Execution Successful ==='''

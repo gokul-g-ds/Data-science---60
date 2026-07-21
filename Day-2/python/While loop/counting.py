4d675a2c0ee90ce6965783e2bedce1a565f41ded
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-21 12:02:25 +0530

    counting.py

---
+++ Day-2/python/While loop/counting.py
@@ -0,0 +1,40 @@
+count = 1
+
+while(count <= 10):
+  print(count)
+  count += 1
+
+#output 
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+=== Code Execution Successful ===
+
+#reverse 
+count = 10
+
+while(count >= 1):
+  print(count)
+  count -= 1
+
+#output 
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+
+=== Code Execution Successful ===

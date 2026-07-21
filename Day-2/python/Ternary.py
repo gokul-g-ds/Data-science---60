d72810dbdee218b82fbf3133b53897c1e57be17b
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-21 10:54:15 +0530

    Ternary.py

---
+++ Day-2/python/Ternary.py
@@ -0,0 +1,11 @@
+age = 15
+result = ("Eligible age" if(age >=18)else "Not Eligible")
+print(result)
+
+'''Not Eligible'''
+
+age = 20
+result = ("Eligible age" if(age >=18)else "Not Eligible")
+print(result)
+
+'''Eligible age'''

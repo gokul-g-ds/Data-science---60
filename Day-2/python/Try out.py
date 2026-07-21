230a9766bd550b65d796152130e21eb80f149312
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-21 10:00:59 +0530

    Try out.py

---
+++ Day-2/python/Try out.py
@@ -0,0 +1,17 @@
+#a^3 + 2bc + c^2
+
+a = int(input("Enter the num a :"))
+b = int(input("Enter the num b :"))
+c = int(input("Enter the num c :"))
+a = a**3
+c = c**2
+b = 2*b*c
+out = a+b+c
+print(out)
+
+#Enter the num a :4
+#Enter the num b :5
+#Enter the num c :6
+#460
+
+=== Code Execution Successful ===

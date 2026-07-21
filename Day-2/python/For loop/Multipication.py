ed18394e653e79d73f0c421a8ebb6dfc4da4c5b7
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-21 11:32:57 +0530

    Multipication.py

---
+++ Day-2/python/For loop/Multipication.py
@@ -0,0 +1,24 @@
+num = int(input("Enter the num :"))
+
+for i in range(2,20,2):
+    print(num,"*",i,num ,"=", num*i)
+
+'''Enter the num :23
+23 * 2 23 = 46
+23 * 4 23 = 92
+23 * 6 23 = 138
+23 * 8 23 = 184
+23 * 10 23 = 230
+23 * 12 23 = 276
+23 * 14 23 = 322
+23 * 16 23 = 368
+23 * 18 23 = 414'''
+
+#sum of num 1 to 10
+
+total = 0;
+for i in range (1,10):
+    total = total+i
+print("sum",total)
+
+'''sum 45'''

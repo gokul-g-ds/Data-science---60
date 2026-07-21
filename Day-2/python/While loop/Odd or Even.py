100cbe7c305e810aa00d45557080f002d1e7771f
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-21 12:06:16 +0530

    Odd or Even.py

---
+++ Day-2/python/While loop/Odd or Even.py
@@ -0,0 +1,37 @@
+count = 2
+
+while(count <= 20):
+  print(count)
+  count += 2
+'''
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20'''
+
+=== Code Execution Successful ===
+
+count = 1
+
+while(count <= 20):
+  print(count)
+  count += 2
+
+'''1
+3
+5
+7
+9
+11
+13
+15
+17
+19'''
+
+=== Code Execution Successful ===

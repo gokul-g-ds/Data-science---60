491b3a2aa2a9b4dabe87804a416329fa47be0794
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-21 10:43:14 +0530

    Nested if.py

---
+++ Day-2/python/Nested if.py
@@ -0,0 +1,20 @@
+num = int(input("Enter the num :"))
+if(num > 0):
+    if(num % 2 == 0):
+        print("positive and even")
+    else:
+        print("positive and odd")
+else:
+    print("The num is negative ")
+
+
+'''Enter the num :34
+positive and even'''
+
+#for negative num 
+'''Enter the num :-23
+The num is negative'''
+
+#for odd num 
+'''Enter the num :55
+positive and odd'''

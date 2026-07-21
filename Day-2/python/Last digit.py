223ae3abebcf479711c63cdf7eb26f540814f1d8
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-21 10:16:17 +0530

    Last digit.py

---
+++ Day-2/python/Last digit.py
@@ -0,0 +1,25 @@
+num = int(input("Enter the num is :"))
+print("unit digit")
+print(num % 10)
+print("Tenth digit")
+print((num // 10)%10)
+print("Hundred digit")
+print((num //100)%10)
+print("Thousand digit")
+print((num / 1000)%10)
+print(type(num))
+print((num // 1000)%10)
+
+'''Enter the num is :123456
+unit digit
+6
+Tenth digit
+5
+Hundred digit
+4
+Thousand digit
+3.456000000000003
+<class 'int'>
+3'''
+
+=== Code Execution Successful ===

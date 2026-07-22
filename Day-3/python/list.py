67d949c5837b9340fe9fe8ba6f31d74cda418c98
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-22 10:07:57 +0530

    list.py

--- Day-3/python/list.py
+++ Day-3/python/list.py
@@ -1,1 +1,36 @@
+# creating list
+lst = [1,2,3,5]
+print(lst)
+
+#diff data types
+print("diff data types")
+lst1 = ["hi","I am","list",1,2,3,5]
+print(lst1)
+print(lst1[1])
+
+#reverse 
+print("using reverse index")
+print(lst[-2])
+
+#add list
+print("adding two list")
+lst2 = lst1 + lst
+print(lst2)
+
+#replication
+print("The replication of list")
+print(lst*4)
+
+'''[1, 2, 3, 5]
+diff data types
+['hi', 'I am', 'list', 1, 2, 3, 5]
+I am
+using reverse index
+3
+adding two list
+['hi', 'I am', 'list', 1, 2, 3, 5, 1, 2, 3, 5]
+The replication of list
+[1, 2, 3, 5, 1, 2, 3, 5, 1, 2, 3, 5, 1, 2, 3, 5]
+
+=== Code Execution Successful ==='''
 

34046c2867b1073736970c88944274010061f571
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-22 12:00:16 +0530

    Traversal and slicing.py

---
+++ Day-3/python/Traversal and slicing.py
@@ -0,0 +1,45 @@
+#traversal 
+
+print("traversal for list")
+number = [10,20,30,40,50]
+for i in range(0,5):
+ print(number[i])
+ 
+print("Traversal for Tuple")
+number_1 = (111,222,333,444)
+for i in range(0,4):
+ print(number_1[i])
+ 
+ #slicing
+ 
+ print("slicing for list")
+ fruits = ["apple","orange","grapes","goa"]
+ print(fruits[0:4])
+ print(fruits[0:4:2])
+ 
+ print("slicing for tuple")
+ Fruits =("apple","orange","grapes","goa")
+ print(Fruits[0:2])
+ print(Fruits[0:4:3])
+
+'''traversal for list
+10
+20
+30
+40
+50
+Traversal for Tuple
+111
+222
+333
+444
+slicing for list
+['apple', 'orange', 'grapes', 'goa']
+['apple', 'grapes']
+slicing for tuple
+('apple', 'orange')
+('apple', 'goa')
+
+=== Code Execution Successful ==='''
+ 
+ 

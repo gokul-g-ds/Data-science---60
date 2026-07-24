ba84ed3520f724c4587cb3a470caf163007f5752
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-24 10:21:36 +0530

    Function.py

--- Day-4 /python/Function.py
+++ Day-4 /python/Function.py
@@ -1,1 +1,20 @@
+#Function 
+
+def greet():
+    name = str(input("Enter your name :"))
+    print("good morning",name)
+    
+greet()    
+
+'''Enter your name :Gokul
+good morning Gokul'''
+
+#fuction with parameter and return 
+def add(a,b):
+    return a+b
+    
+result = add(10,30)
+print(result)
+
+#40
 

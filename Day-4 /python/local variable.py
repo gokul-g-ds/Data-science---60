6182afef0edfe6a1929c9dac818a2fe441880c7d
gokul-g-ds <gokulyo0201@gmail.com>
2026-07-24 11:39:24 +0530

    Scope- local variable.py

---
+++ Day-4 /python/local variable.py
@@ -0,0 +1,27 @@
+#local variable
+
+def my_variable():
+    a = 10
+    b = 20
+    print(a+b)
+    
+my_variable()
+print(a+b)
+
+'''30
+ERROR!
+Traceback (most recent call last):
+  File "<main.py>", line 9, in <module>
+NameError: name 'a' is not defined
+
+=== Code Exited With Errors ==='''
+
+def my_variable():
+    a = 10
+    b = 20
+    print(a+b)
+    
+my_variable()
+#print(a+b)
+
+#30
